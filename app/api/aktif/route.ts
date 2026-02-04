6b0855ee1d78016156aa7baf0d418c64b6339667
VXGN <habbisaputra.y@gmail.com>
2026-02-04 23:36:10 +0700

    Implement GET method for API route with error handling

--- app/api/aktif/route.ts
+++ app/api/aktif/route.ts
@@ -1,6 +1,19 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+export async function GET(request: NextRequest) {
+    try {
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
+
 export async function POST(request: NextRequest) {
-    return NextResponse.json({status: 200, message: "Success"});
+    try {
+        const data = await request.json();
+        return NextResponse.json({status: 200, message: "Success"});
+    } catch (error) {
+        return NextResponse.json({status: 500, message: "Error"});
+    }
 }
