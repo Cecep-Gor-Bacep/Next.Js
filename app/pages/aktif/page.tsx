d9b4927f566bd68a469a5c92d3b272a7db0f9225
ZeiN <zxvw433@gmail.com>
2026-02-11 00:26:53 +0800

    feat: update data fetching logic and enhance row editing functionality in DataGrid component

--- app/pages/aktif/page.tsx
+++ app/pages/aktif/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { getAllMahasiswa } from "@/services/MahasiswaServices";
+import { Height } from "@mui/icons-material";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridRowModel } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
@@ -18,12 +19,20 @@ const columns: GridColDef[] = [
     headerName: "Status",
     width: 110,
     editable: true,
+    valueGetter: (_, row) => row.statusMhs?.[0]?.status || "-",
+    valueSetter: (value, row) => {
+    return { ...row, statusMhs: { ...row.statusMhs, status: value } };
+  },
   },
   {
-    field: "keterangan",
+    field: "keteterangan",
     headerName: "Keterangan",
     width: 200,
     editable: true,
+    valueGetter: (_, row) => row.statusMhs?.[0]?.ket || "-",
+    valueSetter: (value, row) => {
+      return { ...row, statusMhs: { ...row.statusMhs, ket: value } };
+    },
   },
 ];
 
@@ -38,63 +47,63 @@ export default function DataGridDemo() {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const result = await getAllMahasiswa();
-        setRows(result);
+        const result = await fetch("/api/aktif");
+        const data = await result.json();
+        setRows(data);
         setLoading(false);
-      } catch (error) {
-        alert("Terjadi kesalahan saat proses fetching data")
+        console.log(data);
+      } catch (e) {
+        alert("Terjadi kesalahan saat proses fetching data" + e);
       }
     };
 
     fetchData();
-  }, [])
+  }, []);
 
-  if(!mounted){
-    return <Box sx={{height: "90vh", width: "100%"}}/>
+  if (!mounted) {
+    return <Box sx={{ height: "90vh", width: "100%" }} />;
   }
   const handleChange = async (newRow: GridRowModel, oldRow: GridRowModel) => {
-    try{
+    try {
       const response = await fetch("/api/aktif", {
         method: "POST",
-        body: JSON.stringify(newRow)
-      })
+        body: JSON.stringify(newRow),
+      });
 
-      if (!response.ok){
-        throw new Error(response.statusText)
+      if (!response.ok) {
+        throw new Error(response.statusText);
       }
 
       const result = await response.json();
-      console.log(result)
-      
-      return newRow
+      console.log(result);
 
+      return newRow;
     } catch (error) {
-      alert("Terjadi kesalahan saat proses fetching data")
-      return oldRow
+      alert("Terjadi kesalahan saat proses fetching data");
+      return oldRow;
     }
   };
 
   return (
-    <Box sx={{ height: "90vh", width: "100%"}}>
+    <Box sx={{ height: "90vh", width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        getRowId={(row)=>row.nim}
+        getRowId={(row) => row.nim}
         processRowUpdate={handleChange}
-        onProcessRowUpdateError={(error)=>console.log("Edit error: ", error)}
+        onProcessRowUpdateError={(error) => console.log("Edit error: ", error)}
         slotProps={{
-          toolbar: {showQuickFilter: true}
+          toolbar: { showQuickFilter: true },
         }}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: rows.length,
+              pageSize: 25,
             },
           },
         }}
         pageSizeOptions={[5]}
         checkboxSelection
-        
         disableRowSelectionOnClick
       />
     </Box>
