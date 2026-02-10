aeaaa305b6eebea49b72be646c3333908282c609
VXGN <habbisaputra.y@gmail.com>
2026-02-11 00:31:04 +0800

    v.1.4.8

--- api/prismaClient/prisma.ts
+++ api/prismaClient/prisma.ts
@@ -1,21 +1,12 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  //change to reference a table in your schema
-  const val = await prisma.data_mhs.findMany({
+  const val = await prisma.dataMhs.findMany({
     take: 10,
   });
   console.log(val);
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-  process.exit(1);
-});
+export default prisma;
--- app/pages/aktif/page.tsx
+++ app/pages/aktif/page.tsx
@@ -4,27 +4,27 @@ import { getAllMahasiswa } from "@/services/MahasiswaServices";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridRowModel } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_DEFAULT_KEY!
+);
 
 const columns: GridColDef[] = [
-  { field: "nim", headerName: "NIM", width: 150 },
+  { field: "NIM", headerName: "Nim", width: 150 },
   {
-    field: "nama",
+    field: "NAMA",
     headerName: "Nama",
     width: 200,
     editable: true,
   },
   {
-    field: "status",
-    headerName: "Status",
+    field: "thn_masuk",
+    headerName: "Tahun Masuk",
     width: 110,
     editable: true,
-  },
-  {
-    field: "keterangan",
-    headerName: "Keterangan",
-    width: 200,
-    editable: true,
-  },
+  }
 ];
 
 export default function DataGridDemo() {
@@ -38,11 +38,13 @@ export default function DataGridDemo() {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const result = await getAllMahasiswa();
-        setRows(result);
+        const { data, error } = await supabase.from('data_mhs').select();
+        if (error) throw error;
+        setRows((data) as GridRowModel[]);
         setLoading(false);
+        console.log(data);
       } catch (error) {
-        alert("Terjadi kesalahan saat proses fetching data")
+        alert("Terjadi kesalahan saat proses fetching data " + JSON.stringify(error));
       }
     };
 
@@ -52,35 +54,14 @@ export default function DataGridDemo() {
   if(!mounted){
     return <Box sx={{height: "90vh", width: "100%"}}/>
   }
-  const handleChange = async (newRow: GridRowModel, oldRow: GridRowModel) => {
-    try{
-      const response = await fetch("/api/aktif", {
-        method: "POST",
-        body: JSON.stringify(newRow)
-      })
-
-      if (!response.ok){
-        throw new Error(response.statusText)
-      }
-
-      const result = await response.json();
-      console.log(result)
-      
-      return newRow
-
-    } catch (error) {
-      alert("Terjadi kesalahan saat proses fetching data")
-      return oldRow
-    }
-  };
 
   return (
-    <Box sx={{ height: "90vh", width: "100%"}}>
+    <Box sx={{ height: "90vh"}}>
       <DataGrid
         rows={rows}
         columns={columns}
-        getRowId={(row)=>row.nim}
-        processRowUpdate={handleChange}
+        getRowId={(row)=>row.NIM}
+        // processRowUpdate={handleChange}
         onProcessRowUpdateError={(error)=>console.log("Edit error: ", error)}
         slotProps={{
           toolbar: {showQuickFilter: true}
@@ -88,14 +69,13 @@ export default function DataGridDemo() {
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: rows.length,
+              pageSize: 10,
             },
           },
         }}
-        pageSizeOptions={[5]}
+        pageSizeOptions={[rows.length]}
         checkboxSelection
-        
-        disableRowSelectionOnClick
+        // disableRowSelectionOnClick
       />
     </Box>
   );
