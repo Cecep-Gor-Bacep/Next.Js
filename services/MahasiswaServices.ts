90f4b26074f5de9727fb5ed81dff7549b82f66fc
ZeiN <zxvw433@gmail.com>
2026-02-11 09:53:12 +0800

    refactor: streamline data retrieval in MahasiswaServices by removing unused PrismaClient instances

--- services/MahasiswaServices.ts
+++ services/MahasiswaServices.ts
@@ -1,18 +1,38 @@
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "@/lib/prisma";
 
-
-export async function getAllMahasiswa() {
-  const prisma = new PrismaClient()
-
-  const Mhs = await prisma.dataMhs.findMany()
-  
-  return Mhs;
+export async function getAllMhs() {
+  return await prisma.dataMhs.findMany({
+    include: {
+      statusMhs: {
+        select: {
+          status: true,
+          ket: true,
+        },
+      },
+    },
+  });
 }
 
-export async function getStatus() {
-  const prisma = new PrismaClient()
-
-  const status = await prisma.statusMhs.findMany()
-  
-  return status;
+export async function updateStatKet(nim: string, status: string, ket: string) {
+  return await prisma.dataMhs.update({
+    where: {
+      nim: nim,
+    },
+    data: {
+      statusMhs: {
+        updateMany: {
+          where: {
+            nim: nim,
+          },
+          data: {
+            status: status,
+            ket: ket,
+          },
+        },
+      },
+    },
+    include: {
+      statusMhs: true,
+    },
+  });
 }
